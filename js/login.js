39f943cef00a57372a86faac9b5db0ba5753f7e5
Mateus Holanda <90210126+MateusHolandaDev@users.noreply.github.com>
2021-11-13 20:38:31 -0300

    Login

---
+++ js/login.js
@@ -0,0 +1,36 @@
+const input1 = document.querySelector('#t1')
+const input2 = document.querySelector('#t2')
+const label1 = document.querySelector('.txt1')
+const label2 = document.querySelector('.txt2')
+
+console.log(label1)
+console.log(label2)
+console.log(input1)
+console.log(input2)
+
+input1.addEventListener('focus', () => {
+    label1.style.marginBottom = '20px'
+    label1.style.transition = 'all 0.3s'
+})
+
+input2.addEventListener('focus', () => {
+    label2.style.marginBottom = '20px'
+    label2.style.transition = 'all 0.3s'
+})
+
+input1.addEventListener('blur', () => {
+    if(input1.value != ''){
+        label1.style.marginBottom = '20px'
+    }else{
+        label1.style.marginBottom = '0px'
+    }
+    
+})
+
+input2.addEventListener('blur', () => {
+    if(input2.value != ''){
+        label2.style.marginBottom = '20px'
+    }else{
+        label2.style.marginBottom = '0px'
+    }
+})
